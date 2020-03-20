f735b24847ba41f6bf7ada611b35299b81e8d8be
Rohit Naidu <nrohit10@gmail.com>
2020-03-20 23:21:29 +0530

    User Authentication System Functionality Completed

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Python_Practice\User-Authentication-Web-App-With-Python-and-Django\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
